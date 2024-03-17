0ee84476cafc10f367252c5d21d8c646f535a1a8
Dominguez <christian@acivilate.com>
2024-03-17 10:20:06 -0500

    feat: LOGGEDTIME-TASKS fixed loggedtime paths

--- apps/loggedtime/src/loggedtime.repository.ts
+++ apps/loggedtime/src/loggedtime.repository.ts
@@ -3,7 +3,7 @@ import { Logger } from '@nestjs/common';
 import { Model } from 'mongoose';
 import { InjectModel } from '@nestjs/mongoose';
 import { LoggedTimeDocument } from '@loggedtime/models';
-import { CreateLoggedTimedto } from '@loggedtime/dto/create-logged-time.dto';
+import { CreateLoggedTimeDto } from '@loggedtime/dto';
 
 export class LoggedTimeRepository extends AbstractRepository<AbstractDocument> {
   protected readonly logger = new Logger(LoggedTimeRepository.name);
@@ -17,10 +17,10 @@ export class LoggedTimeRepository extends AbstractRepository<AbstractDocument> {
     this.loggedTimeModel = loggedTimeModel;
   }
 
-  async create(loggedTime: CreateLoggedTimedto): Promise<LoggedTimeDocument> {
+  async create(loggedTime: CreateLoggedTimeDto): Promise<LoggedTimeDocument> {
     console.log('INCOMING LOGGED TIME REQUEST TO THE REPOSITORY: ', loggedTime);
     const createdLoggedTime = new this.loggedTimeModel(loggedTime);
-    console.log('created login : ', createdLoggedTime);
+    console.log('created loggedtime : ', createdLoggedTime);
     return await createdLoggedTime.save();
   }
 }
