eae124e3eaa4739e6f23a5c27f6332e1551fe789
Dominguez <christian@acivilate.com>
2024-03-17 10:19:50 -0500

    feat: LOGGEDTIME-TASKS fixed loggedtime paths

--- apps/loggedtime/src/logged-time.service.ts
+++ apps/loggedtime/src/logged-time.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@nestjs/common';
 import { AbstractDocument } from '@app/common/database';
 import { LoggedTimeRepository } from '@loggedtime/loggedtime.repository';
-import { CreateLoggedTimedto } from '@loggedtime/dto';
+import { CreateLoggedTimeDto } from '@loggedtime/dto';
 
 @Injectable()
 export class LoggedTimeService {
   constructor(private loggedTimeRepository: LoggedTimeRepository) {}
 
-  async create(loggedTimeDto: CreateLoggedTimedto): Promise<AbstractDocument> {
+  async create(loggedTimeDto: CreateLoggedTimeDto): Promise<AbstractDocument> {
     console.log('incoming to the service: ', loggedTimeDto);
     return this.loggedTimeRepository.create(loggedTimeDto);
   }
